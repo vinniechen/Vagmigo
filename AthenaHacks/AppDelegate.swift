3e6eaf4785ee34c0e71ef26fb4496449c7e0e073
Vinnie Chen <vinnie.chen211@gmail.com>
2017-04-08 17:35:15 -0700

    Start, uterus, ovary

--- AthenaHacks/AppDelegate.swift
+++ AthenaHacks/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()
+        
+        
+        window?.rootViewController = vc
+        
+        
         return true
     }
 
